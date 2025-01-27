e30812edfe5d877f1f39fd55ef909c524cec8939
Waicung F <better@waicung.net>
2025-01-27 17:56:28 +1100

    viewport size

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col bg-gray-100">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+const viewPortSize = ref({ width: 0, height: 0 });
+
+onMounted(() => {
+  const updateSize = () => {
+    viewPortSize.value = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  };
+
+  window.addEventListener('resize', updateSize);
+  updateSize();
+
+  return () => {
+    window.removeEventListener('resize', updateSize);
+  };
+});
+</script>
+
+<template>
+  <div class="">
+    <div class="flex items-center justify-center h-screen">
+      <text class="text-center">
+      {{ viewPortSize }}
+      </text>
+    </div>
+  </div>
+</template>
