ace870416306d51076662abdc9289218ba33f22d
Waicung F <better@waicung.net>
2025-01-27 17:59:29 +1100

    body size

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <script setup>
 const viewPortSize = ref({ width: 0, height: 0 });
+const bodySize = ref({ width: 0, height: 0 });
 
 onMounted(() => {
   const updateSize = () => {
     viewPortSize.value = {
       width: window.innerWidth,
       height: window.innerHeight,
+    };
+    bodySize.value = {
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
     };
   };
 
@@ -20,10 +25,9 @@ onMounted(() => {
 
 <template>
   <div class="">
-    <div class="flex items-center justify-center h-screen">
-      <text class="text-center">
-      {{ viewPortSize }}
-      </text>
+    <div class="flex items-center justify-center h-screen flex-col">
+      <p>ViewPort{{ viewPortSize }}</p>
+      <p>Body{{ bodySize }}</p>
     </div>
   </div>
 </template>
